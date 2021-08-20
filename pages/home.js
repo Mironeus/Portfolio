ce3aa3a1cde3e029f5b1a8c473a7820690b02fd7
Mironeus <88888010+Mironeus@users.noreply.github.com>
2021-08-21 01:38:22 +0300

    delete page transitions

--- pages/home.js
+++ pages/home.js
@@ -2,7 +2,6 @@ import Grid from '@material-ui/core/Grid';
 import styles from '../styles/home_styles.module.css'
 import { useState } from 'react';
 import { useRouter } from 'next/router';
-import { motion } from 'framer-motion';
 import Link from 'next/link';
 
 
@@ -10,77 +9,74 @@ export default function Home() {
     const [active, setActive] = useState(false);
     const router = useRouter();
     return (
-        <motion.div
-            exit={{ opacity: 0 }}>
-            <div className={styles.page}>
-                <div className={styles.mainNavbarContent}>
-                    <header className={styles.header}>
-                        <nav className={styles.navbar}>
-                            <div className={styles.navLogo}><Link href="/" > Portfolio 2021</Link></div>
-                            <ul className={active ? styles.navMenuActive : styles.navMenu}>
-                                <li className={router.pathname == '/home' ? styles.navItemActive : styles.navItem}>
-                                    <div className={router.pathname == '/home' ? styles.navLinkActive : styles.navLink}>
-                                        <Link href="/home" >
-                                            Home
-                                        </Link>
-                                    </div>
-                                </li>
-                                <li className={router.pathname == '/study' ? styles.navItemActive : styles.navItem}>
-                                    <div className={router.pathname == '/study' ? styles.navLinkActive : styles.navLink}>
-                                        <Link href="/study">
-                                            Study
-                                        </Link>
-                                    </div>
-                                </li>
-                                <li className={router.pathname == '/design' ? styles.navItemActive : styles.navItem}>
-                                    <div className={router.pathname == '/design' ? styles.navLinkActive : styles.navLink}>
-                                        <Link href="/design">
-                                            Design
-                                        </Link>
-                                    </div>
-                                </li>
-                                <li className={router.pathname == '/contacts' ? styles.navItemActive : styles.navItem}>
-                                    <div className={router.pathname == '/contacts' ? styles.navLinkActive : styles.navLink}>
-                                        <Link href="/contacts">
-                                            Contacts
-                                        </Link>
-                                    </div>
-                                </li>
-                            </ul>
-                            <div onClick={() => setActive(!active)} className={active ? styles.hamburgerActive : styles.hamburger}>
-                                <span className={active ? styles.hamburgerLineFirst : styles.bar}></span>
-                                <span className={active ? styles.hamburgerLineSecond : styles.bar}></span>
-                                <span className={active ? styles.hamburgerLineThird : styles.bar}></span>
-                            </div>
-                        </nav>
-                    </header>
-                </div>
-                <div className={styles.mainHomeContent}>
-                    <Grid
-                        container
-                        className={styles.gridContainer}
-                        justifyContent='center'
-                        alignItems='center'
-                    >
-                        <Grid item md={6} lg={5} xl={5} sm={6} >
-                            <img
-                                className={styles.image}
-                                src="/images/portret.jpg" />
-
-                        </Grid>
-                        <Grid item md={10} lg={6} sm={10} className={styles.textBox}>
-                            <div className={styles.title}>
-                                PORTFOLIO
-                            </div>
-                            <div className={styles.welcome}>
-                                Привет.
-                            </div>
-                            <div className={styles.text}> Меня зовут Абрамов Давид и я начинающий web разработчик.  <br />Этот сайт я создал,чтобы рассказать о себе и своих знаниях и возможно пробудить в вас заинтересованность сотрудничества со мной над вашим проектом.</div>
-
-                        </Grid>
-                    </Grid>
-                </div>
+        <div className={styles.page}>
+            <div className={styles.mainNavbarContent}>
+                <header className={styles.header}>
+                    <nav className={styles.navbar}>
+                        <div className={styles.navLogo}><Link href="/" > Portfolio 2021</Link></div>
+                        <ul className={active ? styles.navMenuActive : styles.navMenu}>
+                            <li className={router.pathname == '/home' ? styles.navItemActive : styles.navItem}>
+                                <div className={router.pathname == '/home' ? styles.navLinkActive : styles.navLink}>
+                                    <Link href="/home" >
+                                        Home
+                                    </Link>
+                                </div>
+                            </li>
+                            <li className={router.pathname == '/study' ? styles.navItemActive : styles.navItem}>
+                                <div className={router.pathname == '/study' ? styles.navLinkActive : styles.navLink}>
+                                    <Link href="/study">
+                                        Study
+                                    </Link>
+                                </div>
+                            </li>
+                            <li className={router.pathname == '/design' ? styles.navItemActive : styles.navItem}>
+                                <div className={router.pathname == '/design' ? styles.navLinkActive : styles.navLink}>
+                                    <Link href="/design">
+                                        Design
+                                    </Link>
+                                </div>
+                            </li>
+                            <li className={router.pathname == '/contacts' ? styles.navItemActive : styles.navItem}>
+                                <div className={router.pathname == '/contacts' ? styles.navLinkActive : styles.navLink}>
+                                    <Link href="/contacts">
+                                        Contacts
+                                    </Link>
+                                </div>
+                            </li>
+                        </ul>
+                        <div onClick={() => setActive(!active)} className={active ? styles.hamburgerActive : styles.hamburger}>
+                            <span className={active ? styles.hamburgerLineFirst : styles.bar}></span>
+                            <span className={active ? styles.hamburgerLineSecond : styles.bar}></span>
+                            <span className={active ? styles.hamburgerLineThird : styles.bar}></span>
+                        </div>
+                    </nav>
+                </header>
             </div>
-        </motion.div >
+            <div className={styles.mainHomeContent}>
+                <Grid
+                    container
+                    className={styles.gridContainer}
+                    justifyContent='center'
+                    alignItems='center'
+                >
+                    <Grid item md={6} lg={5} xl={5} sm={6} >
+                        <img
+                            className={styles.image}
+                            src="/images/portret.jpg" />
+
+                    </Grid>
+                    <Grid item md={10} lg={6} sm={10} className={styles.textBox}>
+                        <div className={styles.title}>
+                            PORTFOLIO
+                        </div>
+                        <div className={styles.welcome}>
+                            Привет.
+                        </div>
+                        <div className={styles.text}> Меня зовут Абрамов Давид и я начинающий web разработчик.  <br />Этот сайт я создал,чтобы рассказать о себе и своих знаниях и возможно пробудить в вас заинтересованность сотрудничества со мной над вашим проектом.</div>
+
+                    </Grid>
+                </Grid>
+            </div>
+        </div>
     )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,50 +2,45 @@ import React from 'react';
 import styles from '../styles/index_styles.module.css';
 import { Grid } from '@material-ui/core';
 import Button from 'react-bootstrap/Button';
-import { motion } from 'framer-motion';
 import { useRouter } from 'next/router';
 import Link from 'next/link';
 
 export default function Hello() {
   const router = useRouter();
   return (
-    <motion.div
-      exit={{ opacity: 0 }}
-    >
-      <div className={styles.page}>
-        <Grid
-          container
-          alignItems='center'
-          justifyContent='center'
-        >
-          <Grid item md={12} xs={12}>
-            <div className={styles.logoContainer}>
-              <img
-                className={styles.logo}
-                src="/images/logo.png" alt="David Abramov Logo"
-              />
-            </div>
-          </Grid>
-          <Grid item md={12} xs={12}>
-            <main>
-              <div className={styles.welcome}>HELLO &nbsp;<span className={styles.and}> & </span>&nbsp;WELCOME</div>
-              <div className={styles.myName}> <span className={styles.myNameIs}>MY NAME IS &nbsp;</span> DAVID ABRAMOV</div>
-              <div className={styles.centerButton}>
-                <Button variant='danger' size='lg' className={styles.mainButton} >
-                  <Link href='/home'> ENTER</Link>
-                </Button>
-              </div>
-            </main>
-          </Grid>
-          <Grid md={12} xs={12}>
-            <footer className={styles.footer}>
-              Portfolio&nbsp;2021
-            </footer>
-            <hr width='50%' className={styles.line} />
-          </Grid>
+    <div className={styles.page}>
+      <Grid
+        container
+        alignItems='center'
+        justifyContent='center'
+      >
+        <Grid item md={12} xs={12}>
+          <div className={styles.logoContainer}>
+            <img
+              className={styles.logo}
+              src="/images/logo.png" alt="David Abramov Logo"
+            />
+          </div>
         </Grid>
-      </div >
-    </motion.div>
+        <Grid item md={12} xs={12}>
+          <main>
+            <div className={styles.welcome}>HELLO &nbsp;<span className={styles.and}> & </span>&nbsp;WELCOME</div>
+            <div className={styles.myName}> <span className={styles.myNameIs}>MY NAME IS &nbsp;</span> DAVID ABRAMOV</div>
+            <div className={styles.centerButton}>
+              <Button variant='danger' size='lg' className={styles.mainButton} >
+                <Link href='/home'> ENTER</Link>
+              </Button>
+            </div>
+          </main>
+        </Grid>
+        <Grid md={12} xs={12}>
+          <footer className={styles.footer}>
+            Portfolio&nbsp;2021
+          </footer>
+          <hr width='50%' className={styles.line} />
+        </Grid>
+      </Grid>
+    </div >
   )
 }
 

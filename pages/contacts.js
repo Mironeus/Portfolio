91c04b81abf49ced6f067427ec5d660da852a5b8
david-abramov <sendtoabramov@gmail.com>
2021-11-18 00:42:39 +0300

    Fix links in contacts

--- pages/contacts.js
+++ pages/contacts.js
@@ -1,36 +1,64 @@
 import MainNavbar from "../components/MainNavbar";
-import styles from '../styles/contacts_style.module.css'
-import Grid from '@material-ui/core/Grid';
+import styles from "../styles/contacts_style.module.css";
+import Grid from "@material-ui/core/Grid";
 
 export default function Contacts() {
-    return (
-        <>
-            <div className={styles.page}>
-                <div className={styles.fixedNav}>
-                    <MainNavbar></MainNavbar>
-                </div>
-                <div className={styles.mainContent}>
-                    <Grid
-                        className={styles.text}
-                        container
-                        alignItems='flex-end'
-                        justifyContent='flex-end'
-                        direction='row'
-                    >
-                        <Grid item md={4}>
-                            <div className={styles.contactsText}>
-                                <span className={styles.span}>EMAIL: <br /> </span>sendtoabramov@gmail.com <br />
-                                <span className={styles.span}>GIT: <br /> </span>github.com/abramov-david <br />
-                                <span className={styles.span}>BEHANCE: <br /></span>behance.net/abramov204bc78 <br />
-                                <span className={styles.span}>PHONE <span className={styles.phoneCity}>(Russia,Saint-Petersburg)</span>: <br /> </span> +7-904-614-93-29 <br />
-                            </div>
-                            <div className={styles.copyRight}>
-                                © 2021 David Abramov All Rights Reserved
-                            </div>
-                        </Grid>
-                    </Grid>
-                </div>
-            </div >
-        </>
-    )
+  return (
+    <>
+      <div className={styles.page}>
+        <div className={styles.fixedNav}>
+          <MainNavbar></MainNavbar>
+        </div>
+        <div className={styles.mainContent}>
+          <Grid
+            className={styles.text}
+            container
+            alignItems="flex-end"
+            justifyContent="flex-end"
+            direction="row"
+          >
+            <Grid item md={4}>
+              <div className={styles.contactsText}>
+                <span className={styles.span}>
+                  EMAIL: <br />{" "}
+                </span>{" "}
+                <a href="mailto:sendtoabramov@gmail.com">
+                  sendtoabramov@gmail.com
+                </a>
+                <br />
+                <span className={styles.span}>
+                  GIT: <br />{" "}
+                </span>
+                <a href="https://github.com/abramov-david">
+                  {" "}
+                  github.com/abramov-david
+                </a>
+                <br />
+                <span className={styles.span}>
+                  BEHANCE: <br />
+                </span>
+                <a href="https://www.behance.net/abramov204bc78">
+                  {" "}
+                  behance.net/abramov204bc78
+                </a>
+                <br />
+                <span className={styles.span}>
+                  PHONE{" "}
+                  <span className={styles.phoneCity}>
+                    (Russia,Saint-Petersburg)
+                  </span>
+                  : <br />{" "}
+                </span>{" "}
+                <a href="tel:+7-904-614-93-29">+7-904-614-93-29</a>
+                <br />
+              </div>
+              <div className={styles.copyRight}>
+                © 2021 David Abramov All Rights Reserved
+              </div>
+            </Grid>
+          </Grid>
+        </div>
+      </div>
+    </>
+  );
 }
